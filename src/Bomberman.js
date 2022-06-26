9cfd68f46652b30e23763e1d14527142b0ff7e73
Felipe Alves <103237188+ffelipealves@users.noreply.github.com>
2022-06-26 12:26:04 -0300

    moviment

--- src/Bomberman.js
+++ src/Bomberman.js
@@ -1,3 +1,4 @@
+import MovingDirection from "./Moviment.js";
 export default class Bomberman {
   constructor(x, y, tileSize, velocity, tileMap) {
     this.x = x;
@@ -5,10 +6,17 @@ export default class Bomberman {
     this.tileSize = tileSize;
     this.velocity = velocity;
     this.tileMap = tileMap;
+
+    this.currentMovingDirection = null;
+    this.requestMovingDirection = null;
+
+    document.addEventListener("keydown", this.#keydown);
+
     this.#loadBombermanImages();
   }
 
   draw(ctx) {
+    this.#move();
     ctx.drawImage(
       this.bombermanImages[this.bombermanImageIndex],
       this.x,
@@ -39,5 +47,57 @@ export default class Bomberman {
     ];
 
     this.bombermanImageIndex = 0;
+  }
+
+  #keydown = (event) => {
+    //up
+    if (event.keyCode == 38) {
+      if (this.currentMovingDirection == MovingDirection.down)
+        this.currentMovingDirection = MovingDirection.up;
+      this.requestMovingDirection = MovingDirection.up;
+    }
+    //down
+    if (event.keyCode == 40) {
+      if (this.currentMovingDirection == MovingDirection.up)
+        this.currentMovingDirection = MovingDirection.down;
+      this.requestMovingDirection = MovingDirection.down;
+    }
+    //left
+    if (event.keyCode == 37) {
+      if (this.currentMovingDirection == MovingDirection.right)
+        this.currentMovingDirection = MovingDirection.left;
+      this.requestMovingDirection = MovingDirection.left;
+    }
+    //right
+    if (event.keyCode == 39) {
+      if (this.currentMovingDirection == MovingDirection.left)
+        this.currentMovingDirection = MovingDirection.right;
+      this.requestMovingDirection = MovingDirection.right;
+    }
+  };
+
+  #move() {
+    if (this.currentMovingDirection !== this.requestMovingDirection) {
+      if (
+        Number.isInteger(this.x / this.tileSize) &&
+        Number.isInteger(this.y / this.tileSize)
+      ) {
+        this.currentMovingDirection = this.requestMovingDirection;
+      }
+    }
+    switch(this.currentMovingDirection){
+      case MovingDirection.up:
+      this.y -= this.velocity;
+      break;
+      case MovingDirection.down:
+      this.y += this.velocity;
+      break;
+      case MovingDirection.left:
+      this.x -= this.velocity;
+      break;
+      case MovingDirection.right:
+      this.x += this.velocity;
+      break;
+    }
   }
 }
